e1fdaa77d8f3989ab581c531dca2a69df433c8ec
zzh <syncbad@outlook.com>
2022-04-24 15:51:12 +0800

    push

---
+++ footer.php
@@ -0,0 +1,1 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?><footer id="footer">&copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a><p>Keep calm and carry on.</p></footer></div><?php $this->footer(); ?></body></html>
